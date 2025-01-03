6cfa1601fca01eaa3b079aeadca98e65841c35f5
nicof-fulmar <156094170+nicof-fulmar@users.noreply.github.com>
2025-01-03 13:46:04 +0200

    Clean Repo + Github Package

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,59 @@
+import java.util.Properties
+
 plugins {
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
+}
+
+val githubProperties = Properties().apply {
+    load(File("${layout.projectDirectory.asFile}/github.properties").inputStream())
+}
+
+android {
+
+    namespace = "com.supermegazinc.${project.name}"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            run {
+                groupId = "com.supermegazinc.libraries"
+                artifactId = artifactId
+                version = "1.0"
+                artifact("${layout.buildDirectory.get().asFile}/outputs/aar/$artifactId-release.aar")
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/supermegazinc/Android-Logger-Library")
+            credentials {
+                username = githubProperties["gpr.usr"] as String?
+                password = githubProperties["gpr.key"] as String?
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
 }
--- logger/src/main/java/com/supermegazinc/logger/Logger.kt
+++
@@ -1,9 +0,0 @@
-package com.supermegazinc.logger
-
-import android.util.Log
-
-interface Logger {
-    fun d(tag: String?, message: String)
-    fun i(tag: String?, message: String)
-    fun e(tag: String?, message: String)
-}
